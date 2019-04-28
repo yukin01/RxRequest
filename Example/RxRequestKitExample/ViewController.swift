395ad9a72b07fef5a16bd38a7e432d57b4024069
yukin01 <bump1105@gmail.com>
2019-04-28 17:52:32 +0900

    feat: add sample project

---
+++ Example/RxRequestKitExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RxRequestKitExample
+//
+//  Created by Kinjo on 2019/04/28.
+//  Copyright © 2019 yukin01. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
