54797b963868cdb6ea873ead32534c3509f5103e
yukin01 <bump1105@gmail.com>
2019-04-28 17:17:24 +0900

    feat: create macos project

---
+++ RxRequestKit/RxRequestKit.h
@@ -0,0 +1,19 @@
+//
+//  RxRequestKit.h
+//  RxRequestKit
+//
+//  Created by Kinjo on 2019/04/28.
+//  Copyright © 2019 yukin01. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for RxRequestKit.
+FOUNDATION_EXPORT double RxRequestKitVersionNumber;
+
+//! Project version string for RxRequestKit.
+FOUNDATION_EXPORT const unsigned char RxRequestKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RxRequestKit/PublicHeader.h>
+
+
